84ce73f0882058909f8e677cd08fe9136392e44c
emmanuelwithme <wow90327@gmail.com>
2022-11-17 03:24:55 +0800

    merge everyone code finally done

--- app.py
+++ app.py
@@ -250,5 +250,95 @@ def monthlyGrowthRateVS():
         graphJSON.append(json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder))
 
     return flask.render_template("monthlyGrowthRateVS.html",graphJSON1=graphJSON[0],graphJSON2=graphJSON[1],graphJSON3=graphJSON[2],city1=city1,district1=district1,industry1=industry1,city2=city2,district2=district2,industry2=industry2)
+
+# 分析該地區該產業的折線圖(3個折線圖)(包括平均發票開立張數、金額、客單價)
+@app1.route("/page3",methods=['POST','GET'])
+def page3():
+    #存在空值，即删除该行  #重置索引值.reset_index(drop=True)--drop=True刪除原來索引值
+    raw_dataset=data.dropna(axis=0, how='any',inplace=False).reset_index(drop=True)
+
+    temp=[]
+
+    if flask.request.method =='POST':
+        if flask.request.values['send']=='送出': #收取表單資訊
+            select1 = flask.request.form.get(key='city',type=str)
+            select2 = flask.request.form.get(key='district',type=str)
+            select3 = flask.request.form.get(key='industry',type=str)
+
+            filter1=(raw_dataset["縣市名稱"]==select1)
+            filter2=(raw_dataset["鄉鎮市區名稱"]==select2)
+            filter3=(raw_dataset["行業名稱"]==select3)
+
+            temp=raw_dataset[filter1&filter2&filter3].reset_index(drop=True).loc[:,['發票年月','平均開立張數','平均開立金額','平均客單價']]
+            time=[]
+            for i in temp["發票年月"]:
+                i=str(i)
+                time.append(i[0:4]+"-"+i[4:6]) #["2020","08"]
+            #時間格式不對
+
+            temp={"time":time,"num":list(temp["平均開立張數"]),"amount":list(temp["平均開立金額"]),"price":list(temp["平均客單價"])}
+            temp= pandas.DataFrame(temp)
+            temp = temp.sort_values(by="time")
+            
+            if len(temp)==0:#查無結果--NotFound
+                return "<form method='post' action='/page3'><title>page3:</title><p>縣市:"+select1+"</p><p>鄉鎮市區:"+select2+"</p><p>產業:"+select3+"</p><input type ='button' onclick='history.back()' value='回到上一頁'></input></form>"+"<img src='https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'>"
+            else:
+                #1
+                fig = px.line(temp, x="time", y="num", title="產業平均開立張數折線圖")
+                graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+                #2
+                fig = px.line(temp, x="time", y="amount", title="產業平均開立金額折線圖")
+                graphJSON2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+                #3
+                fig = px.line(temp, x="time", y="price", title="產業平均客單價折線圖")
+                graphJSON3 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+                return "<form method='post' action='/page3'><title>page3:</title><p>縣市:"+select1+"</p><p>鄉鎮市區:"+select2+"</p><p>產業:"+select3+"</p><input type ='button' onclick='history.back()' value='回到上一頁'></input></form>"+"<div id='line-chart1' class='chart' ”></div><div id='line-chart2' class='chart' ”></div><div id='line-chart3' class='chart' ”></div><script src='https://cdn.plot.ly/plotly-latest.min.js'></script><script type='text/javascript'>var graphs = "+graphJSON1+";Plotly.plot('line-chart1', graphs, {});</script><script type='text/javascript'>var graphs = "+graphJSON2+";Plotly.plot('line-chart2', graphs, {});</script><script type='text/javascript'>var graphs = "+graphJSON3+";Plotly.plot('line-chart3', graphs, {});</script>"
+        return "send error!"
+    return flask.render_template('page3.html')
+
+@app1.route('/pieChartBSelectDistrict',methods=['GET'])
+def pieChartBSelectDistrict():
+    return flask.render_template("pieChartBSelectDistrict.html")
+
+@app1.route('/pieChartB',methods=['GET'])
+def pieChartB():
+    city =  flask.request.args.get("city")
+    industry = flask.request.args.get("industry")
+    print("你抓的東西是",city,"和",industry)
+    af = data[data["縣市名稱"].isin([city]) & data["行業名稱"].isin([industry]) ]
+    fig01 = px.pie(af,values=af['平均開立張數'], names=af['鄉鎮市區名稱'],  title='plotly pie Chart', color='鄉鎮市區名稱')
+    graphJSON1 = json.dumps(fig01, cls=plotly.utils.PlotlyJSONEncoder)
+    fig02 = px.pie(af,values=af['平均開立金額'], names=af['鄉鎮市區名稱'],  title='plotly pie Chart', color='鄉鎮市區名稱')
+    graphJSON2 = json.dumps(fig02, cls=plotly.utils.PlotlyJSONEncoder)
+    fig03 = px.pie(af,values=af['平均客單價'], names=af['鄉鎮市區名稱'],  title='plotly pie Chart', color='鄉鎮市區名稱')
+    graphJSON3 = json.dumps(fig03, cls=plotly.utils.PlotlyJSONEncoder)
+    return flask.render_template("pieChartB.html",graphJSON1=graphJSON1,graphJSON2=graphJSON2,graphJSON3=graphJSON3,city=city,industry=industry)
+
+@app1.route('/pieChartASelectDistrict',methods=['GET'])
+def pieChartASelectDistrict():
+    return flask.render_template("pieChartASelectDistrict.html")
+
+@app1.route('/pieChartA',methods=['GET'])
+def pieChartA():
+    #將index中選單的資料傳進來
+    city =  flask.request.args.get("city")
+    district = flask.request.args.get("district")
+    #print("你抓的東西是",area01,"和",area02)-->這是單純測試值是否有抓近來
+    #利用Pandas DataFrame中的方法抓到想要的值
+    af = data[data["縣市名稱"].isin([city]) & data["鄉鎮市區名稱"].isin([district])]
+
+    #將各個圓餅圖的資料傳進graphJSON中
+    fig = px.pie(af,values=af['平均開立張數'], names=af['行業名稱'], title='平均開立張數', color='行業名稱')
+    graphJSON1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    fig2 = px.pie(af,values=af['平均開立金額'], names=af['行業名稱'],  title='平均開立金額', color='行業名稱')
+    graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    fig3 = px.pie(af,values=af['平均客單價'], names=af['行業名稱'],  title='平均客單價', color='行業名稱')
+    graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+    #最後一次傳進plotly_charts.html網頁中
+    return flask.render_template("pieChartA.html",graphJSON1=graphJSON1,graphJSON2=graphJSON2,graphJSON3=graphJSON3,city=city,district=district)
+
 if __name__ == "__main__":
     app1.run()
